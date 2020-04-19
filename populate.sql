21e4aa80cf0204dd68b69eb5c4fe74d4a74ce97a
Чебан Ростислав <44871917+ChebanRostislavKM81@users.noreply.github.com>
2020-04-19 19:36:25 +0300

    Delete populate.sql

--- populate.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Neutral(dates, home_team, away_team, home_score, away_score, tournament, city, country, neutral)
-VALUES('26.02.56', 'Wales', 'England', 0, 2, 'Friendly', 'Cardiff', 'Wales', FALSE);
-INSERT INTO Neutral(dates, home_team, away_team, home_score, away_score, tournament, city, country, neutral)
-VALUES('27.02.56', 'Belgium', 'Hungary', 3, 1, 'Friendly', 'Gent', 'Belgium', FALSE);
-INSERT INTO Neutral(dates, home_team, away_team, home_score, away_score, tournament, city, country, neutral)
-VALUES('28.02.56', 'Denmark', 'Sweden', 0, 0, 'Friendly', 'Copenhavn', 'Denmark', FALSE);
