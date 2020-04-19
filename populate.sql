17926dac272e4db2dc24dca316d8768716eb0ced
Чебан Ростислав <44871917+ChebanRostislavKM81@users.noreply.github.com>
2020-04-19 19:36:51 +0300

    Add files via upload

---
+++ populate.sql
@@ -0,0 +1,18 @@
+INSERT INTO Matches(dates, home_team, away_team, home_score, away_score, tournament)
+VALUES('26.02.56', 'Wales', 'England', 0, 2, 'Friendly');
+INSERT INTO Matches(dates, home_team, away_team, home_score, away_score, tournament)
+VALUES('27.02.56', 'Belgium', 'Hungary', 3, 1, 'Friendly');
+INSERT INTO Matches(dates, home_team, away_team, home_score, away_score, tournament)
+VALUES('28.02.56', 'Denmark', 'Sweden', 0, 0, 'Friendly');
+INSERT INTO Neutral(dates, home_team, away_team, neutral)
+VALUES('26.02.56', 'Wales', 'England', 'FALSE');
+INSERT INTO Neutral(dates, home_team, away_team, neutral)
+VALUES('27.02.56', 'Belgium', 'Hungary', 'FALSE');
+INSERT INTO Neutral(dates, home_team, away_team, neutral)
+VALUES('28.02.56', 'Denmark', 'Sweden', 'TRUE');
+INSERT INTO CITIES(dates, tournament, city, country)
+VALUES('26.02.56', 'Friendly', 'Cardiff', 'Wales');
+INSERT INTO CITIES(dates, tournament, city, country)
+VALUES('27.02.56', 'Friendly', 'Genk', 'Belgium');
+INSERT INTO CITIES(dates, tournament, city, country)
+VALUES('28.02.56', 'UEFA Cup', 'London', 'Great Britain');
